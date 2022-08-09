3b587f93cc25e4183fb474c39be9b7b65dcd87c8
= <vapradeep817@gmail.com>
2022-08-09 15:22:21 -0500

    Add form and modal

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import BookDisplay from './BookDisplay.js';
 import axios from 'axios';
-import { Button, Modal } from 'react-bootstrap';
+import { Button, Modal, Form } from 'react-bootstrap';
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       books: [],
-      show: false
+      show: false,
+      newBookData: {
+        title: '',
+        description: '',
+        status: ''
+      }
     }
   }
 
@@ -22,13 +27,26 @@ class BestBooks extends React.Component {
   handleClose = () => {
     this.setState({
       show: false
-    })
-  }
+    });
+  };
+  
   handleShow = () => {
     this.setState({
       show: true
-    })
-  }
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      show: false,
+      newBookData: {
+        title: event.target.title.value,
+        description: event.target.description.value,
+        status: event.target.status.value
+      }
+    });
+  };
 
   render() {
     return (
@@ -51,15 +69,30 @@ class BestBooks extends React.Component {
           <Modal.Header closeButton>
             <Modal.Title>Modal heading</Modal.Title>
           </Modal.Header>
-          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={this.handleClose}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={this.handleClose}>
-              Save Changes
-            </Button>
-          </Modal.Footer>
+          <Form onSubmit={this.handleSubmit}>
+            <Modal.Body>
+              <Form.Group controlId="title">
+                <Form.Label>Title</Form.Label>
+                <Form.Control type="text" />
+              </Form.Group>
+              <Form.Group controlId="description">
+                <Form.Label>Description</Form.Label>
+                <Form.Control type="text" />
+              </Form.Group>
+              <Form.Group controlId="status">
+                <Form.Label>Status</Form.Label>
+                <Form.Control type="text" />
+              </Form.Group>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={this.handleClose}>
+                Close
+              </Button>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Modal.Footer>
+          </Form>
         </Modal>
       </>
     )
