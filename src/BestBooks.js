572ed7c1b1cac88d6fd28e3c9b377d02b6c8210f
= <vapradeep817@gmail.com>
2022-08-09 14:43:53 -0500

    Update formatting

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import BookDisplay from './BookDisplay.js';
 import axios from 'axios';
-import { Button,Modal} from 'react-bootstrap';
+import { Button, Modal } from 'react-bootstrap';
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       books: [],
-      show:false,
+      show: false
     }
   }
 
@@ -19,19 +19,17 @@ class BestBooks extends React.Component {
     });
   }
 
-
   handleClose = () => {
     this.setState({
-      show:false
+      show: false
     })
   }
   handleShow = () => {
     this.setState({
-      show:true
+      show: true
     })
   }
 
- 
   render() {
     return (
       <>
@@ -43,26 +41,26 @@ class BestBooks extends React.Component {
           })
         ) : (
           <h3>No Books Found :( </h3>
-          
-        )}
-         <Button variant="primary" onClick={this.handleShow}>
-        Launch demo modal
-      </Button>
 
-      <Modal show={this.state.show} onHide={this.handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={this.handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={this.handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
+        )}
+        <Button variant="primary" onClick={this.handleShow}>
+          Launch demo modal
+        </Button>
+
+        <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={this.handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </>
     )
   }
