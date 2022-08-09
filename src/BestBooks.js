e024b94859b5d53e0a7342e47f2b10581ee6b93b
MARY <marymwihaki123@gmail.com>
2022-08-09 14:40:24 -0500

    added Modal

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import BookDisplay from './BookDisplay.js';
 import axios from 'axios';
-
+import { Button,Modal} from 'react-bootstrap';
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: []
+      books: [],
+      show:false,
     }
   }
 
@@ -18,6 +19,19 @@ class BestBooks extends React.Component {
     });
   }
 
+
+  handleClose = () => {
+    this.setState({
+      show:false
+    })
+  }
+  handleShow = () => {
+    this.setState({
+      show:true
+    })
+  }
+
+ 
   render() {
     return (
       <>
@@ -28,8 +42,27 @@ class BestBooks extends React.Component {
             return <BookDisplay key={book._id} title={book.title} description={book.description} status={book.status} />
           })
         ) : (
-          <h3>No Books Found :(</h3>
+          <h3>No Books Found :( </h3>
+          
         )}
+         <Button variant="primary" onClick={this.handleShow}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={this.state.show} onHide={this.handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={this.handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={this.handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
       </>
     )
   }
